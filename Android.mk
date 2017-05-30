d2bcd20c5f970bddda3c5652e94f5f2d51898e68
adeii <adeii@dr.com>
2017-05-30 23:24:45 +0200

    audio-caf msm7x27a

---
+++ Android.mk
@@ -0,0 +1,13 @@
+ifeq ($(call my-dir),$(call project-path-for,qcom-audio))
+
+ifneq ($(filter msm7x27a,$(TARGET_BOARD_PLATFORM)),)
+
+MY_LOCAL_PATH := $(call my-dir)
+
+ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+include $(MY_LOCAL_PATH)/legacy/Android.mk
+endif
+
+endif
+
+endif
